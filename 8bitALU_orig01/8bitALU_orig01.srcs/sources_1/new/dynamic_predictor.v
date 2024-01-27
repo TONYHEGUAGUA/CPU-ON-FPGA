37b929c2ea379bd507f400de98fef83a41ce45c6
Hu Yan <hvdanyan@gmail.com>
2024-01-27 20:41:38 +0800

    first commit

---
+++ 8bitALU_orig01/8bitALU_orig01.srcs/sources_1/new/dynamic_predictor.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/01/27 20:13:40
+// Design Name: 
+// Module Name: dynamic_predictor
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+module dynamic_predictor（
+
+）;
+    // Add your code here
+    
+endmodule
